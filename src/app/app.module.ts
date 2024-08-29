baa08f55f2ce6bef3e26fa4b13c8b2e38802f806
dalmo <facurymanoel10@gmail.com>
2024-08-29 16:07:40 -0300

    Criando a Classe AppConstants as rotas e a Barra de Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,22 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import {FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+
+export const appRouters: Routes = [
+
+  //{path: 'usuarioAdd', component: UsuarioAddComponent},
+  //{path: 'usuarioAdd/:id', component: UsuarioAddComponent},
+  //{path: 'usuarioProduto', component: UsuarioComponent}
+];
+
+export const routes : ModuleWithProviders = RouterModule.forRoot(appRouters);
+
 
 @NgModule({
   declarations: [
@@ -13,7 +25,9 @@ import {FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
